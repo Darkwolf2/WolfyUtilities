0a53ebb4a417168fec5477bfa3e845a2f076cc5f
Darkwolf2 <47623686+Darkwolf2@users.noreply.github.com>
2019-02-14 01:13:12 -0700

    Added src folder for my minecraft mod

---
+++ src/main/java/com/superwolfman19/wolfyutilities/Main.java
@@ -0,0 +1,13 @@
+package com.superwolfman19.wolfyutilities;
+
+import com.superwolfman19.wolfyutilities.util.Reference;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.Instance;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
+public class Main {
+
+	@Instance
+	public static Main instance;
+}
---
+++ src/main/java/com/superwolfman19/wolfyutilities/util/Reference.java
@@ -0,0 +1,12 @@
+package com.superwolfman19.wolfyutilities.util;
+
+public class Reference {
+
+	public static final String MOD_ID = "wu";
+	public static final String NAME = "Wolfy Utilities";
+	public static final String VERSION = "Alpha 0.0.2.13";
+	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
+	public static final String CLIENT_PROXY_CLASS = "";
+	public static final String COMMON_PROXY_CLASS = "";
+	
+}
