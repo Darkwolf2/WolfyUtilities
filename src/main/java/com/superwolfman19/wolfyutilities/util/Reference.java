28c13aaa35c109f87751a0c016122d2566c0b724
Darkwolf2 <47623686+Darkwolf2@users.noreply.github.com>
2019-02-15 00:16:28 -0700

    Updated Reference.java file with updated code

--- src/main/java/com/superwolfman19/wolfyutilities/util/Reference.java
+++ src/main/java/com/superwolfman19/wolfyutilities/util/Reference.java
@@ -1,12 +1,13 @@
 package com.superwolfman19.wolfyutilities.util;
 
-public class Reference {
+public class Reference 
+{
 
 	public static final String MOD_ID = "wu";
 	public static final String NAME = "Wolfy Utilities";
-	public static final String VERSION = "Alpha 0.0.2.13";
+	public static final String VERSION = "Alpha 0.0.28.124";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
-	public static final String CLIENT_PROXY_CLASS = "";
-	public static final String COMMON_PROXY_CLASS = "";
+	public static final String CLIENT_PROXY_CLASS = "com.superwolfman19.wolfyutilities.proxy.ClientProxy";
+	public static final String COMMON_PROXY_CLASS = "com.superwolfman19.wolfyutilities.proxy.CommonProxy";
 	
 }
